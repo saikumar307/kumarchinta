1be68ff55bd866ba3514ad02ee2906a9bdc42747
saikumar307 <chsaikumar307@gmail.com>
2022-08-18 18:02:42 +0530

    saikumar pushed

---
+++ src/org/DataTypes/DataTypes.java
@@ -0,0 +1,25 @@
+package org.DataTypes;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte age = 22;
+		System.out.println("my name is " + age);
+		short pinNo = 1234;
+		System.out.println("ATM no " + pinNo);
+		int i = 123456;
+		System.out.println("i" + 100);
+		long phNo = 1234567895l;
+		System.out.println(phNo);
+		float sal = 3456.2365895565f;
+		System.out.println(sal);
+		double avg = 96.235645;
+		System.out.println(avg);
+		char gen = 'm';
+		System.out.println(gen);
+		boolean perEmp = true;
+		System.out.println(perEmp);
+		String email = "example@gmail.com";
+		System.out.println(email);
+	}
+	
+}
